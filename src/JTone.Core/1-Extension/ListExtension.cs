666b657b491424464db9aa6a1addb595da96e3ba
tanwei <tanwei@jzfz.com.cn>
2021-03-15 15:24:40 +0800

    扩展list

---
+++ src/JTone.Core/1-Extension/ListExtension.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+
+namespace JTone.Core
+{
+    public class ListExtension
+    {
+        /// <summary>
+        /// Count扩展 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public int Count<T>(IEnumerable<T> data)
+        {
+            return data?.Count() ?? 0;
+        }
+    }
+}
